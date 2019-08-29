aefd58de5a994804951e17b04377b4aaaf5896fa
xiaoxieaichirou <13226512215@163.com>
2019-08-30 00:24:17 +0800

    pythob接口测试学习

--- api_test/conftest.py
+++ api_test/conftest.py
@@ -1,10 +1,14 @@
 import pytest, os
 from Environment import Env
+from configparser import ConfigParser
 
-
+'''通过fixture做测试环境的切换'''
 @pytest.fixture(scope="module", autouse=True)
 def env():
-    yield Env(token=os.environ['token'])
+    config = ConfigParser()
+    config.read('data.ini')
+    api_root_url = config['test_env']['api_root_url']
+    yield Env(api_root_url=api_root_url, token=os.environ['token'])
 
 @pytest.fixture(scope="module", autouse=True)
 def just_print():
